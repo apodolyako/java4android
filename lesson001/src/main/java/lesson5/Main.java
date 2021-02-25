f29809aa9ca2889ec7e4268669fcbb09086846b1
Andrey.Podolyako <7775917@mail.ru>
2021-02-25 18:55:57 +0300

    Выполняю задание5

---
+++ lesson001/src/main/java/lesson5/Main.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class Main {
+
+    public static void main(String[] args){
+            Personal[] persArr = new Personal[5];
+            persArr[0] = new Personal("Иванов", "Инженер", 35 );
+            persArr[1] = new Personal("Путин", "Президент", 60 );
+            persArr[2] = new Personal("Лавров", "Министр", 65 );
+            persArr[3] = new Personal("Пупкин Василий", "Специалист по Медиа ", 25 );
+            persArr[4] = new Personal("Пирогов", "Программист", 40 );
+
+
+
+
+    }
+}
---
+++ lesson001/src/main/java/lesson5/Personal.java
@@ -0,0 +1,34 @@
+package lesson5;
+
+public class Personal {
+    private String name;
+    private String pos;
+    private String email;
+    private String phone;
+    private int age;
+    private int salary;
+
+    public Personal(String name, String pos, String email, String phone, int age, int salary){
+        this.name = name;
+        this.pos = pos;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+        this.salary = salary;
+
+    }
+
+    public Personal(String name, String pos, int age){
+        this(name, pos, 'unknown','unknown', age, 100000 )
+
+    }
+
+    public void printStuffInfo(){
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Имя сотрудника: " + name + "Должность: " + pos + "Email: " + email + "Телефон: " + phone + "Возраст: " + age + "Зарплата: " + salary;
+    }
+}
